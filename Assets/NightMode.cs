e160a63759cce79b49ee1df006d46b0fb1c4dabf
ElviraMishra <84223693+ElichkaM@users.noreply.github.com>
2024-02-21 19:08:56 +0530

    NightSwitching is Working

---
+++ Assets/NightMode.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using OculusSampleFramework;
+
+
+
+public class NightMode : MonoBehaviour
+{
+    [SerializeField] private Material[] skyboxMaterials; // Array of skybox materials to cycle through
+    [SerializeField] private UnityEvent[] events; // Unity events to trigger when changing skybox
+
+    private int currentIndex = 0; // Index of the current skybox material
+    private bool isGrabbing = false; // Flag to track if the object is being grabbed
+
+    private void Update()
+    {
+        if (isGrabbing && OVRInput.Get(OVRInput.Button.PrimaryHandTrigger)) // Check if the primary hand trigger is pressed while grabbing
+        {
+            ChangeSkybox();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("hand")) // Assuming your Oculus Quest Controller's collider is tagged as "Hand"
+        {
+            isGrabbing = true; // Set the flag to true when the controller collider enters the object's collider
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("hand"))
+        {
+            isGrabbing = false; // Reset the flag to false when the controller collider exits the object's collider
+        }
+    }
+
+    public void ChangeSkybox()
+    {
+        currentIndex = (currentIndex + 1) % skyboxMaterials.Length; // Cycle to the next skybox material
+        RenderSettings.skybox = skyboxMaterials[currentIndex]; // Set the new skybox material
+
+        if (events.Length > currentIndex && events[currentIndex] != null)
+        {
+            events[currentIndex].Invoke(); // Invoke the corresponding Unity event
+        }
+    }
+}
